7e6d0d4a77bab6f4632206810a33e69c2c62b70e
eryuruk <gunceeryuruk@gmail.com>
2023-04-28 19:50:36 -0400

    Completed module

---
+++ ssg/site.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import typer
+from ssg.site import Site
+
+class Site:
+    
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+      
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+    
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create.dir(path)
+        
+def main(source="content", dest="dist"):
+    config = {"source": source, "dest": dest  }
+    isite = Site(**config) 
+    isite.build()      
+
+typer.run(main)
