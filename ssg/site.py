881e8c8db2937f0af847714bdf10657dca9bf97a
eryuruk <gunceeryuruk@gmail.com>
2023-04-28 20:03:52 -0400

    Completed prj 1 mod 1 - take 2

--- ssg/site.py
+++ ssg/site.py
@@ -14,5 +14,5 @@ class Site:
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
-                self.create.dir(path)
+                self.create_dir(path)
         
