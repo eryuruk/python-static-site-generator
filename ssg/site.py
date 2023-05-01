9c50d5fc7ba62fe158e90ffa0e35bcf0068df4a6
eryuruk <gunceeryuruk@gmail.com>
2023-05-01 10:47:48 -0400

    my new comment

--- ssg/site.py
+++ ssg/site.py
@@ -5,7 +5,7 @@ class Site:
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
-
+#my comment
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
@@ -14,4 +14,4 @@ class Site:
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
-                self.create_dir(path)     
+                self.create_dir(path)    
